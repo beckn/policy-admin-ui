5cfd589fd468bcbcdf32ced56d753178318b171f
Ujjwal Tiwari <ujjwaltiwari@Ujjwals-MacBook-Pro.local>
2023-02-28 15:12:13 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { BrowserRouter, NavLink } from "react-router-dom";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <header>Hello app</header>
+      <nav>
+      <NavLink to="">Home</NavLink>
+        <NavLink to="about">About</NavLink>
+        <NavLink to="contact">Contact</NavLink>
+      </nav>
+    </BrowserRouter>
   );
 }
 
