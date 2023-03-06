e786242e9885ebbd298231318f3681f9d966680b
Ujjwal Tiwari <ujjwaltiwari@192.168.1.2>
2023-03-06 21:11:53 +0530

    Jenkins upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
-import { BrowserRouter, createBrowserRouter, NavLink } from "react-router-dom";
 import "./App.css";
-import Header from "./Layouts/Header/Header";
 import SideNav from "./Layouts/SideNav/SideNav";
-import Card from "./Components/Card/Card";
 
 //This is a test push
 
